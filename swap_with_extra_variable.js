753515e2685bf53d55ad2a612c2e3c7404b3b661
Sirajuddin Khan <sirajuddinkhan7718@gmail.com>
2026-04-14 14:44:26 +0530

    Day 1

---
+++ swap_with_extra_variable.js
@@ -0,0 +1,9 @@
+// Swap with extra variable
+let a=10
+let b=20
+console.log("Before swapping, values of a & b: ",a,b);
+
+let c=a
+a=b
+b=c
+console.log("After Swapping, values of a & b: ",a,b)
---
+++ swap_without_extra_varabile.js
@@ -0,0 +1,8 @@
+//Swap Without extra variable
+a=19
+b=21
+console.log("Before swapping, the values of a & b were: ",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+console.log("After swapping, the values of a & b are: ",a,b);
